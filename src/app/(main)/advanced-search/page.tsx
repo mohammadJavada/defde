22156f3f1e5f357db635f45376bc55ea8eeb30e8
mohammadjavad <mohammadjavada78@gmail.com>
2023-11-19 19:30:28 +0330

    sssss

--- src/app/(main)/advanced-search/page.tsx
+++ src/app/(main)/advanced-search/page.tsx
@@ -1,5 +1,4 @@
 import GetStaticDatasAPI from "@/apis/static-datas/get-static-data.api";
-import GetStaticDatasNotSSRAPI from "@/apis/static-datas/get-static-datas-not-ssr.api";
 import AdvancedSearch from "@/page/advanced-search";
 
 export default async function AdvancedSearchPage() {
@@ -7,15 +6,6 @@ export default async function AdvancedSearchPage() {
     endPoint: "/Color/Get/All",
     method: "get",
   });
-  let postedData = {
-    page_number: 1,
-    page_size: 200,
-  };
 
-  // const brandData = await GetStaticDatasNotSSRAPI({
-  //   endPoint: "/BrandModelType/Get/All",
-  //   data: postedData,
-  //   method: "post",
-  // });
   return <AdvancedSearch colors={colors} models={[]} />;
 }
