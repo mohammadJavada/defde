8bf62f71a78e8443e9ddce3d8bf172c447a7e660
mohammadjavad <mohammadjavada78@gmail.com>
2023-11-19 18:58:44 +0330

    v

--- src/app/(main)/vehicle-check/page.tsx
+++ src/app/(main)/vehicle-check/page.tsx
@@ -1,18 +1,32 @@
 import GetStaticDatasAPI from "@/apis/static-datas/get-static-data.api";
 import GetStaticDatasNotSSRAPI from "@/apis/static-datas/get-static-datas-not-ssr.api";
+import { FRONT2DB } from "@/config/url";
 import VehicleCheck from "@/page/vehicle-check";
 import React from "react";
+
+async function postData(data = {}) {
+  const response = await fetch(`${FRONT2DB}/BrandModelType/Get/All`, {
+    method: "POST",
+    cache: "force-cache",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+  return response.json();
+}
 
 const page = async () => {
   let postedData = {
     page_number: 1,
     page_size: 200,
   };
-  const brandData = await GetStaticDatasNotSSRAPI({
-    endPoint: "/BrandModelType/Get/All",
-    data: postedData,
-    method: "post",
-  });
+  // const brandData = await GetStaticDatasNotSSRAPI({
+  //   endPoint: "/BrandModelType/Get/All",
+  //   data: postedData,
+  //   method: "post",
+  // });
+  const brandData = await postData(postedData);
   return (
     <div>
       <VehicleCheck cars={brandData} />
